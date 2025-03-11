3a1474ac117890e6f6391b4578723097e062bd8e
Mario David <mario.david@faktorzehn.de>
2025-03-11 13:32:19 +0100

    update otel collector; health checks

--- build.sh
+++ build.sh
@@ -2,19 +2,11 @@
 
 set -e
 
-rm -rf ./policy-app/build/libs/
-./gradlew :policy-core:policy-core:clean
-./gradlew :policy-app:clean
+./gradlew :policy-core:policy-core:clean :policy-app:clean
 ./gradlew -Pvaadin.productionMode=true :policy-app:bootJar --no-build-cache
 
-rm -rf ./quote-app/build/libs/
-./gradlew :quote-core:quote-core:clean
-./gradlew :quote-app:clean
+./gradlew :quote-core:quote-core:clean :quote-app:clean
 ./gradlew -Pvaadin.productionMode=true :quote-app:bootJar --no-build-cache
 
-rm -rf ./account-app/build/libs/
-./gradlew :account-core:account-core:clean
-./gradlew :account-app:clean
+./gradlew :account-core:account-core:clean :account-app:clean
 ./gradlew -Pvaadin.productionMode=true :account-app:bootJar --no-build-cache
-
-
